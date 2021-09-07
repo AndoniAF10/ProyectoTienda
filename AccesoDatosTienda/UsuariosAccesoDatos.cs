e3560934140164a124182a671d20db877260aa65
AndoniAF10 <cesarandoniafanadorjazoandoni@hotmail.com>
2021-09-07 15:01:14 -0500

    dado de alta

---
+++ AccesoDatosTienda/UsuariosAccesoDatos.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EntidadesTienda;
+
+namespace AccesoDatosTienda
+{
+    public class UsuariosAccesoDatos
+    {
+        ConexionAccesoDatos _conexion;
+        public UsuariosAccesoDatos()
+        {
+
+            try
+            {
+                _conexion = new ConexionAccesoDatos("localhost", "root", "123456789", "tienda3", 3306);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Fallo la conexion" + ex.Message);
+            }
+
+            
+        }
+
+        public void GuardarCategorias(Productos producto)
+        {
+            try
+            {
+                string consulta = string.Format("insert into producto values('{0}','{1}','{2}','{3}')",
+                    producto.IdProducto, producto.Nombre,producto.Descripcion,producto.Precio);
+                _conexion.EjecutarConsulta(consulta);
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine("Fallo el guardado" + ex.Message);
+            }
+        }
+    }
+}
---
+++ ManejadoresTienda/ManejaTienda.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EntidadesTienda;
+using AccesoDatosTienda;
+
+namespace ManejadoresTienda
+{
+    public class ManejaTienda
+    {
+        UsuariosAccesoDatos _usuariosAccesoDatos = new UsuariosAccesoDatos();
+
+        public Tuple<bool, string> ValidarCategorias(Productos producto)
+        {
+            bool error = true;
+            string cadenaErrores = "";
+
+            if (producto.IdProducto == null)
+            {
+                cadenaErrores = cadenaErrores + "El campo del ID no puede ser vacio \n";
+                error = false;
+            }
+
+            if (producto.Nombre.Length == 0 || producto.Nombre == null)
+            {
+                cadenaErrores = cadenaErrores + "El campo del Nombre no puede ser vacio \n";
+                error = false;
+            }
+            if (producto.Descripcion.Length == 0 || producto.Descripcion == null)
+            {
+                cadenaErrores = cadenaErrores + "El campo de la descripcion no puede ser vacio \n";
+                error = false;
+            }
+
+            if (producto.Precio == null)
+            {
+                cadenaErrores = cadenaErrores + "El campo del Precio no puede ser vacio \n";
+                error = false;
+            }
+
+
+            var valida = new Tuple<bool, string>(error, cadenaErrores);
+            return valida;
+        }
+
+        public void GuardarCategorias(Productos producto)
+        {
+            try
+            {
+                _usuariosAccesoDatos.GuardarCategorias(producto);
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine("Fallo el guardado" + ex.Message);
+            }
+        }
+    }
+}
